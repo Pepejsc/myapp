218957edc0345a7845e70717117eeb26e5c39554
edosnondbz <edosnondbz@gmail.com>
2024-08-27 21:59:18 +0000

    new commit on repo

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+// Suggested code may be subject to a license. Learn more: ~LicenseLog:319759868.
+    return const MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text('Hello World'),
+        ),
+      ),
+    );
+  }
+}
